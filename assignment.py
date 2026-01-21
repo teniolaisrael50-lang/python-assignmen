6eff3da3a4e955a14c7c7d38b780c52431a2967b
teniolaisrael50-lang <teniolaisrael50@gmail.com>
2026-01-21 21:54:43 +0100

    Add Python practice script for basic operations

---
+++ assignment.py
@@ -0,0 +1,17 @@
+# My Python Practice Script
+print("Welcome to my Python assignment")
+
+name = "Ayodele"
+age = 17
+print("My name is", name)
+print("I am", age, "years old")
+
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+user_name = input("Enter your name: ")
+print("Hello", user_name)
